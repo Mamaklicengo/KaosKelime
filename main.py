58f151eb18ba4b6f1a57f94b38a4cfb49d5d7862
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2024-01-28 19:37:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2444,7 +2444,7 @@ async def iptal(message):
             kelime = f(f"games.{oyun_id}.kelime")
             oyun_tipi = f(f"games.{oyun_id}.oyun_tipi")
 
-            if (oyun_tipi == "kelimeoyunu" or oyun_tipi == "sessiz_sinema"):
+            if (oyun_tipi == "kelimeoyunu"):
                 skorlar = f(f"games.{oyun_id}.skorlar")
                 #round = int(f(f"games.{oyun_id}.round")) + 1
                 #toplam_round = f(f"games.{oyun_id}.toplam_round")
