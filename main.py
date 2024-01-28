382c729d55d3f85d2c0288e207adc3ead5bb637f
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2024-01-28 19:59:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2482,6 +2482,31 @@ Kazananlar 👑
                 keyboard.add(callback_button)
 
                 await bot.send_message(chat_id, f"💥 Oyun başarıyla iptal edildi! Cevap: {kelime}", reply_markup=keyboard)
+                skorlar = dict(sorted(skorlar.items(), key=lambda item: item[1]))
+                skorlar_list = list(skorlar)[::-1]
+
+                metin = f"""❗️ Oyun Durduruldu
+
+Kazananlar 👑
+"""
+                for n, i in enumerate(skorlar_list):
+                    if n + 1 == 1:
+                        metin += "🥇 "
+                    elif n + 1 == 2:
+                        metin += "🥈 "
+                    elif n + 1 == 3:
+                        metin += "🥉 "
+                    else:
+                        metin += "▫️ "
+
+                    skorlar[i] = round(skorlar[i])
+                    metin += f'<b>{n+1}.</b> {f(f"privates.{i}.first_name")} → <code>{skorlar[i]}</code> puan'
+                    
+                    metin += "\n"
+                keyboard = types.InlineKeyboardMarkup()
+                callback_button = types.InlineKeyboardButton(text="Tekrar oyna 🔃", callback_data="sessizsinema")
+                keyboard.add(callback_button)
+                await bot.send_message(chat_id, metin, reply_markup=keyboard)               
             #f(f"games.{oyun_id}", "$del")
             oyunu_iptal_et(oyun_id)
             await log_gonder(user_id=user_id, chat_id=chat_id, eylem="iptal etti")
